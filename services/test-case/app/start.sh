1305a175eabaa6523971390506c9e26bac3870b3
Marten Jacobs <mail@vanmarten.nl>
2020-12-14 16:03:40 +0100

    Less random port and baud rate selection

--- services/test-case/app/start.sh
+++ services/test-case/app/start.sh
@@ -1,9 +1,9 @@
-stty -F /dev/FTDI.0 115200 sane
+stty -F /dev/FTDI.0 9600 sane
 stty -F /dev/FTDI.1 9600 sane
-stty -F /dev/FTDI.3 115200 sane
+stty -F /dev/FTDI.2 9600 sane
 
-cat /dev/FTDI.1 &
-cat /dev/FTDI.3 &
 cat /dev/FTDI.0 &
+cat /dev/FTDI.1 &
+cat /dev/FTDI.2 &
 
 sleep infinity
